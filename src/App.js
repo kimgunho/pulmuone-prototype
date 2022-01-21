e0be2141b1f4d36e37407cf9ba65a0f4619ddba0
kim <yuyukgh@naver.com>
2022-01-21 14:26:15 +0900

    Feat : 추가 header 컴포넌트

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import styles from './App.module.scss';
 import { links } from './assets/data/links';
 
+import Header from './components/shared/Header';
 import Home from './pages/home';
 
 const cx = classNames.bind(styles);
@@ -11,6 +12,7 @@ const cx = classNames.bind(styles);
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className={cx('wrapper')}>
         <Routes>
           <Route path={links.home} element={<Home />} />
---
+++ src/assets/data/gnb.js
@@ -0,0 +1,30 @@
+import { links } from './links';
+
+export const gnb_left = [
+  {
+    title: '화강암반수',
+    link: links,
+  },
+  {
+    title: '브랜드',
+    link: links,
+  },
+  {
+    title: '제품안내',
+    link: links,
+  },
+];
+export const gnb_right = [
+  {
+    title: 'CSR',
+    link: links,
+  },
+  {
+    title: '고객센터',
+    link: links,
+  },
+  {
+    title: '회사소개',
+    link: links,
+  },
+];
