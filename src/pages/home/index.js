04493a5f11b340cd4ec00188070c03a2d2cf8bd2
kim <yuyukgh@naver.com>
2022-01-28 02:34:35 +0900

    Feat : 추가 home 중앙물방울

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,33 @@
+import { useRef, useEffect } from 'react';
+
 import S01 from '../../components/home/S01';
 import S02 from '../../components/home/S02';
 
+import './index.scss';
+
+import waterDrop from '../../assets/images/home/water.png';
+
 function Home() {
+  const rangeRef = useRef();
+  const waterDropRef = useRef();
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      waterDropRef.current.classList.add('active');
+      setTimeout(() => {
+        waterDropRef.current.classList.remove('active');
+      }, 500);
+    });
+  }, []);
+
   return (
-    <>
+    <div ref={rangeRef} style={{ height: 99999, backgroundColor: 'black' }}>
+      <div className="waterDrop">
+        <img ref={waterDropRef} src={waterDrop} alt="물방울" />
+      </div>
       <S01 />
       <S02 />
-    </>
+    </div>
   );
 }
 
