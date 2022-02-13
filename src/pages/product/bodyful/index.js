10b8422d046806ee71a7353a4b7ab321a2e25539
kim <yuyukgh@naver.com>
2022-02-13 23:33:33 +0900

    Feat : 서브 제품소개 바디풀  업데이트

--- src/pages/product/bodyful/index.js
+++ src/pages/product/bodyful/index.js
@@ -2,10 +2,32 @@ import classNames from 'classnames/bind';
 
 import styles from './index.module.scss';
 
+import PageTitle from '../../../components/shared/PageTitle';
+import S01 from '../../../components/product/bodyful/S01';
+import S02 from '../../../components/product/bodyful/S02';
+import S03 from '../../../components/product/bodyful/S03';
+import S04 from '../../../components/product/bodyful/S04';
+import S05 from '../../../components/product/bodyful/S05';
+import Outro from '../../../components/shared/Outro';
+
 const cx = classNames.bind(styles);
 
 function Bodyful() {
-  return <div className={cx('container')}>Bodyful</div>;
+  return (
+    <div className={cx('container')}>
+      <PageTitle title="풀무원샘물 바디풀" sub="마시는 것 까지 운동이다!" />
+      <S01 />
+      <S02 />
+      <S03 />
+      <S04 />
+      <S05 />
+      <Outro
+        title="프리미엄 해양심층수, 바디풀/n
+건강한 수분 섭취를 시작해 보세요!"
+        color="blue"
+      />
+    </div>
+  );
 }
 
 export default Bodyful;
