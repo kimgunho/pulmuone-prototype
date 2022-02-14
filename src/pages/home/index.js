04017de1a6c3eaaf11a18a2dccf40c49f76843c5
Donghwa Lee <moralmk@gmail.com>
2022-02-14 23:11:32 +0900

    홈 구성 섹션 콤포넌트 파일 디렉토리 재배치

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,12 +3,12 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import './index.scss';
-import S01 from '../../components/home-new/S01';
-import S02 from '../../components/home-new/S02';
-import S03 from '../../components/home-new/S03';
-import S04 from '../../components/home-new/S04';
-import S05 from '../../components/home/S05';
-import S06 from '../../components/home/S06';
+import S01 from './S01';
+import S02 from './S02';
+import S03 from './S03';
+import S04 from './S04';
+import S05 from './S05';
+import S06 from './S06';
 
 gsap.registerPlugin(ScrollTrigger);
 
