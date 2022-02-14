8d76c2f4c9b4d9fbfdb8123a060417390ceb5e5c
kim <yuyukgh@naver.com>
2022-02-15 02:14:59 +0900

    Feat : 서브 회사소개 풀무원  업데이트

--- src/pages/gree/pulmuone/index.js
+++ src/pages/gree/pulmuone/index.js
@@ -2,10 +2,20 @@ import classNames from 'classnames/bind';
 
 import styles from './index.module.scss';
 
+import PageTitle from '../../../components/shared/PageTitle';
+import S01 from '../../../components/gree/pulmuone/S01';
+import S02 from '../../../components/gree/pulmuone/S02';
+
 const cx = classNames.bind(styles);
 
 function Pulmuone() {
-  return <div className={cx('container')}>Pulmuone</div>;
+  return (
+    <div className={cx('container')}>
+      <PageTitle title={'풀무원샘물'} />
+      <S01 />
+      <S02 />
+    </div>
+  );
 }
 
 export default Pulmuone;
