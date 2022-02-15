161fc4588fadd091410ec4f7bf723a6d9ffc1348
kim <yuyukgh@naver.com>
2022-02-15 15:28:40 +0900

    Feat : 404페이지 컴포넌트 업데이트

--- src/components/shared/NotPage.js
+++ src/components/shared/NotPage.js
@@ -5,7 +5,13 @@ import styles from './NotPage.module.scss';
 const cx = classNames.bind(styles);
 
 function NotPage() {
-  return <div className={cx('container')}>not page......</div>;
+  return (
+    <div className={cx('container')}>
+      <div className={cx('limiter')}>
+        <p>존재하지 않는 페이지입니다</p>
+      </div>
+    </div>
+  );
 }
 
 export default NotPage;
