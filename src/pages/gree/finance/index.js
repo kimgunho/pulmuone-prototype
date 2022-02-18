375047d7ef054bf6504eaafff0eec736425d5650
kim <yuyukgh@naver.com>
2022-02-18 13:59:15 +0900

    Feat : 전자공시 상태관리 데이터화

--- src/pages/gree/finance/index.js
+++ src/pages/gree/finance/index.js
@@ -3,26 +3,34 @@ import classNames from 'classnames/bind';
 
 import styles from './index.module.scss';
 
-import { finance, finances } from '../../../assets/data/finance';
+import { finances } from '../../../assets/data/finance';
 
 import PageTitle from '../../../components/shared/PageTitle';
 
 const cx = classNames.bind(styles);
 
 function Finance() {
-  const [data, setData] = useState(finances);
+  const [year, setYear] = useState(2020);
+  const [data, setData] = useState(finances.filter((item) => item.year === year));
 
-  const handleChange = (event) => {};
+  useEffect(() => {
+    const thisYearData = finances.filter((item) => item.year === year);
+    setData(thisYearData);
+  }, [year]);
+
+  const onChange = (event) => {
+    const { value } = event.target;
+    setYear(Number(value));
+  };
 
   return (
     <div className={cx('container')}>
       <PageTitle title={'전자공시'} />
       <div className={cx('selectInner')}>
         <div className={cx('select')}>
-          <select>
+          <select onChange={onChange}>
             <option value={2020}>2020 전자공시</option>
             <option value={2019}>2019 전자공시</option>
-            <option value={2018}>2018 전자공시</option>
           </select>
           <button>확인하기</button>
         </div>
@@ -31,21 +39,21 @@ function Finance() {
       <div className={cx('section')}>
         <div className={cx('limiter')}>
           <div className={cx('table')}>
-            <p className={cx('sub')}>전자공시 - 18기 결산 공고</p>
+            <p className={cx('sub')}>전자공시 - {data[0].finance}기 결산 공고</p>
             <h3 className={cx('title')}>재무상태표</h3>
 
-            <p className={cx('date')}>2020년 12월 31일 현재(단위 :원)</p>
+            <p className={cx('date')}>{data[0].date} 현재(단위 :원)</p>
             <div className={cx('head')}>
               <ul>
                 <li>과목</li>
-                <li>제 18(당) 기</li>
-                <li>제 17(전) 기</li>
+                <li>제 {data[0].finance}(당) 기</li>
+                <li>제 {data[0].finance - 1}(전) 기</li>
               </ul>
             </div>
 
             <div className={cx('body')}>
               <ul>
-                {finance.map((item, index) => (
+                {data[0].data.map((item, index) => (
                   <li key={index}>
                     <ul>
                       <li className={cx({ point: item.point })}>{item.subject}</li>
