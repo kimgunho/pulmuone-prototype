c74adbfe583cfabf33db04706bb3c53251d4d8c4
kim <yuyukgh@naver.com>
2022-02-22 15:51:29 +0900

    Design : Table 반응형 스타일

--- src/components/product/Table.js
+++ src/components/product/Table.js
@@ -8,30 +8,32 @@ const Table = ({ table }) => {
   return (
     <div className={cx(['container', table.color === 'green' ? 'green' : 'blue'])}>
       <h3 className={cx('title')}>{table.title}</h3>
-      <ul className={cx('head')}>
-        {table.point ? <li className={cx('side')}>null</li> : null}
-        {table.head.map((text, index) => (
-          <li key={index}>{text}</li>
-        ))}
-      </ul>
+      <div className={cx('table')}>
+        <ul className={cx('head')}>
+          {table.point ? <li className={cx('side')}>null</li> : null}
+          {table.head.map((text, index) => (
+            <li key={index}>{text}</li>
+          ))}
+        </ul>
 
-      <ul className={cx('body')}>
-        {table.body.map((list, index) => (
-          <li key={index}>
-            <ul>
-              {list.data.map((text, index) => {
-                return table.point ? (
-                  <li className={cx({ point: index === 0 })} key={index}>
-                    {text}
-                  </li>
-                ) : (
-                  <li key={index}>{text}</li>
-                );
-              })}
-            </ul>
-          </li>
-        ))}
-      </ul>
+        <ul className={cx('body')}>
+          {table.body.map((list, index) => (
+            <li key={index}>
+              <ul>
+                {list.data.map((text, index) => {
+                  return table.point ? (
+                    <li className={cx({ point: index === 0 })} key={index}>
+                      {text}
+                    </li>
+                  ) : (
+                    <li key={index}>{text}</li>
+                  );
+                })}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
