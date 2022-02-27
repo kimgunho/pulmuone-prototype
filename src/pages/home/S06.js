1d2ffc7e4fbab00057fb9ebfb43b6dd76ee9a674
Donghwa Lee <moralmk@gmail.com>
2022-02-28 04:13:01 +0900

    홈 반응형 소스코드 개선

--- src/pages/home/S06.js
+++ src/pages/home/S06.js
@@ -14,15 +14,12 @@ const S06 = () => {
           <h2>
             국내 먹는샘물 업계 최초 <br /> AWS 인증
           </h2>
-
           <div className={cx('logo')}>
             <img src={aws} alt="" />
           </div>
-
           <p>
             수자원의 지속 가능성에 관심을 가지고 이를 보존하기 위한 노력을 인정 받아
-            <br />
-            국내 먹는샘물 업계 최초로 AWS certification 인증을 획득했습니다.{' '}
+            <br /> 국내 먹는샘물 업계 최초로 AWS certification 인증을 획득했습니다.{' '}
           </p>
           <Link to="/CSR/수자원-보호">CSR 바로가기</Link>
         </div>
